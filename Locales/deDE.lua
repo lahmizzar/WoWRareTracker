26c41863e82ef5080a311520e30fae0502863679
Lahmizzar <lahmizzar@devxive.com>
2018-09-17 18:05:20 +0200

    Adding lang support for locales

---
+++ Locales/deDE.lua
@@ -0,0 +1,17 @@
+-- Localization file for deDE by Lahmizzar (valeryon.tv/lahmizzar)
+local L = LibStub("AceLocale-3.0"):NewLocale("WoWRareTracker", "deDE")
+if not L then return end
+
+--Debug
+L["string1"] = "Zeichenkette1"
+
+--Rares
+L["Burning Goliath"] = "Brennder Goliath"
+
+--TomTom
+
+--DataBroker
+
+--Prints to Chat
+
+--UnitFrames
---
+++ Locales/enUS.lua
@@ -0,0 +1,17 @@
+-- Localization file for enUS and enGB.
+local L = LibStub("AceLocale-3.0"):NewLocale("WoWRareTracker", "enUS", true)
+
+--Debug
+L["string1"] = true
+
+--Config
+
+--Rares
+
+--TomTom
+
+--DataBroker
+
+--Prints to Chat
+
+--UnitFrames
